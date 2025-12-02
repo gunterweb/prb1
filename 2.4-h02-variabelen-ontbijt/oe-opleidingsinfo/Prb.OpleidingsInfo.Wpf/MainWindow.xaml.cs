4ad4df96123b0befa6b2bbd3e57d9e6854091317
Gunter Robbrecht <gunter.robbrecht@student.howest.be>
2025-12-02 20:25:30 +0100

    oef 2.4 Variable Types

--- 2.4-h02-variabelen-ontbijt/oe-opleidingsinfo/Prb.OpleidingsInfo.Wpf/MainWindow.xaml.cs
+++ 2.4-h02-variabelen-ontbijt/oe-opleidingsinfo/Prb.OpleidingsInfo.Wpf/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace Prb.OpleidingsInfo.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        string naamOpleiding;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +29,43 @@ namespace Prb.OpleidingsInfo.Wpf
         private void TxtKostOpleiding_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cmbAantalStudenten.Items.Add(20);
+            cmbAantalStudenten.Items.Add(21);
+            cmbAantalStudenten.Items.Add(22);
+            cmbAantalStudenten.Items.Add(23);
+            cmbAantalStudenten.Items.Add(24);
+            cmbAantalStudenten.Items.Add(25);
+
+            cmbAantalStudenten.SelectedIndex = 0;
+        }
+
+        private void BtnInfoOpleiding_Click(object sender, RoutedEventArgs e)
+        {
+            //naam van de opleiding, locatie, kost.
+            string naamOpleiding = txtOpleiding.Text;
+            string locatieOpleiding = txtGemeente.Text;
+            decimal kostOpleiding = decimal.Parse(txtKostOpleiding.Text);
+
+            //            string samenvatting = $"Naam opleiding: { naamOpleiding}\nLocatie: { locatieOpleiding}\nKostOpleiding: {kostOpleiding.ToString("0.00")}\n\n ";
+
+            string samenvatting = $"Naam opleiding: {naamOpleiding}";
+            samenvatting += $"\nlocatieOpleiding: {locatieOpleiding}";
+            samenvatting += $"\nkostOpleiding: {kostOpleiding.ToString("0.00")} ";
+
+            tbkToonInfo.Text = samenvatting;
+            
+
+        }
+
+        private void BtnInfoStudenten_Click(object sender, RoutedEventArgs e)
+        {
+            //naam opleiding, aantal studenten, gemiddelde leeftijd, aantal hersencellen.
+            NaamOpleiding = txtOpleiding.Text;
+            int aantalStudenten = (int)cmbAantalStudenten.SelectedItem;
         }
     }
 }
